33a8c372302437138dbff4317ba0ee747499b208
Ashish Rathore <rathorashish79@gmail.com>
2023-03-06 22:34:36 +0530

    docs: corrected spelling mistakes

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { to } from "../utils/helper.js";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
 mongoose.set("debug", true);
@@ -8,22 +8,22 @@ mongoose.set("debug", true);
 let db_url = process.env.MongoURLLocal;
 
 export async function getConnection() {
-    try {
-        let [err, connection] = await to(
-            mongoose.connect(db_url, {
-                useUnifiedTopology: true,
-                useNewUrlParser: true,
-            })
-        );
-        if (err) {
-            console.log("Error occures db");
-            throw err;
-        }
-        if (connection) {
-            console.log("Connected to database successfully");
-        }
-        return Promise.resolve(connection);
-    } catch (e) {
-        throw e;
+  try {
+    let [err, connection] = await to(
+      mongoose.connect(db_url, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      })
+    );
+    if (err) {
+      console.log("Error occurs db");
+      throw err;
     }
+    if (connection) {
+      console.log("Connected to database successfully");
+    }
+    return Promise.resolve(connection);
+  } catch (e) {
+    throw e;
+  }
 }
