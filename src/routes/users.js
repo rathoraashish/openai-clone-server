8f593507863d78e4121851ce6b4fcb83f2643f2e
Ashish Rathore <rathorashish79@gmail.com>
2023-03-07 21:46:45 +0530

    docs: updated comments for user routing

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,19 +3,34 @@ const router = express.Router();
 import { UserController } from "../controllers/userController.js";
 import { validateUser } from "../middlewares/userAuth.js";
 
-// Add new user route
+/**
+ * POST /add
+ * Add new user route
+ */
 router.post("/add", new UserController().addUser);
 
-// User login route
+/**
+ * POST /login
+ * User login route
+ */
 router.post("/login", new UserController().login);
 
-// Add new prompt for user
+/**
+ * POST /add-prompt
+ * Add new prompt for user
+ */
 router.post("/add-prompt", validateUser, new UserController().addNewPrompt);
 
-// Get all prompts
+/**
+ * GET /prompts
+ * Get all prompts
+ */
 router.get("/prompts", validateUser, new UserController().getAllUserPrompts);
 
-//Get prompt by prompt id
+/**
+ * GET /prompt
+ * Get prompt by prompt id
+ */
 router.get("/prompt", validateUser, new UserController().getPromptById);
 
 export { router };
